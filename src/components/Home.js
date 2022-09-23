2156e56dbec097a58b9d6b661eec3a5e52e9b36b
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-23 08:26:18 +0300

    made some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ function Home() {
   // update the JSX being returned!
   return ( 
 
-  <div id ="Home">
-    
+  <div id ="home">
+
     <h1 style={{color:'firebrick'}}> {name} is a Web Developer from {city} </h1>
 
  </div>
